6872f0b9119c93f8d18ff8ba3794fa8ad95e9747
25one <25one@ukr.net>
2018-09-25 12:38:19 +0300

    Update Start.php

--- models/Start.php
+++ models/Start.php
@@ -8,12 +8,10 @@ use yii\db\ActiveRecord;
 class Start extends ActiveRecord
 {
 
-    //public $id_projects;
-
     public function rules()
     {
        return [
-          //[['name'], 'required', 'message' => 'поле ФИО-обязательно'],
+          //[['name'], 'safe'],
        ];
     }
 
